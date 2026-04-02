3027644148698b36e077639c4a24465fa1731790
Xavi Planas <xavi.planas.peres@gmail.com>
2026-04-02 13:52:23 +0200

    Add inserción de tablas modified

--- src/titanic_load_to_db.py
+++ src/titanic_load_to_db.py
@@ -6,6 +6,7 @@ from titanic_utils import mysql_engine, postgres_engine
 
 # Leer dataset Titanic
 df = pd.read_csv("data/raw/titanic.csv")
+df_modified = pd.read_csv("data/modified/titanic_modified.csv")
 
 # Mapear tipos de columna para SQL
 dtype_mapping = {
@@ -27,11 +28,14 @@ def load_to_db(df, table_name, engine, dtype_mapping):
     try:
         df.to_sql(name=table_name, con=engine, if_exists='replace', index=False, dtype=dtype_mapping)
         print(f"{table_name} cargada correctamente en {engine.url.database}")
+        
     except Exception as e:
         print(f"Error cargando {table_name} en {engine.url.database}: {e}")
 
 # Cargar en MySQL
 load_to_db(df, "titanic", mysql_engine, dtype_mapping)
+load_to_db(df, "titanic_modified", mysql_engine, dtype_mapping)
 
 # Cargar en PostgreSQL
 load_to_db(df, "titanic", postgres_engine, dtype_mapping)
+load_to_db(df, "titanic_modified", postgres_engine, dtype_mapping)
