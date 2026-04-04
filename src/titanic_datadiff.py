c3003c7b2fe820a7bb811ad3045e882e859030c2
Xavi Planas <xavi.planas.peres@gmail.com>
2026-04-04 22:02:11 +0200

    comparación por todas las columnas de la tabla

--- src/titanic_datadiff.py
+++ src/titanic_datadiff.py
@@ -4,27 +4,40 @@ from sqlalchemy import inspect
 
 disable_tracking()
 
-src_table = dataset["raw"]["table"]
-dst_table = dataset["modified"]["table"]
+table_raw = dataset["raw"]["table"]
+table_modified = dataset["modified"]["table"]
+
+mysql_metadata = inspect(mysql_engine)
+mysql_all_columns=[col["name"] for col in mysql_metadata.get_columns(table_raw)]
+#mysql_all_columns = []
 
 table_mysql = connect_to_table(
     f"mysql://{mysql_engine_url}",
-    table_name=src_table,
-    key_columns=("PassengerId")
+    table_name=table_raw,
+    key_columns=("PassengerId"),
+    extra_columns = mysql_all_columns
 )
 
 table_pg = connect_to_table(
     f"postgresql://{postgres_engine_url}",
-    table_name=dst_table,
-    key_columns=("PassengerId")
+    table_name=table_modified,
+    key_columns=("PassengerId"),
+    extra_columns = mysql_all_columns
 )
 
-mysql_metadata = inspect(mysql_engine)
+added = 0
+removed = 0
+total = 0
+#for diff in diff_tables(table_mysql, table_pg, extra_columns=mysql_all_columns):
+for diff in diff_tables(table_mysql, table_pg):
+   total+=1
+   if diff[0] == '+' :
+      added+=1
+   elif diff[0] == '-' :
+      removed+=1
 
-mysql_all_columns=[col["name"] for col in mysql_metadata.get_columns(src_table)]
-print(mysql_all_columns)
+   print(diff)
 
-for diff in diff_tables(table_mysql, table_pg, extra_columns=mysql_all_columns):
-    print(diff)
-
-#diff_result = diff_tables(table_mysql, table_pg) 
+print("Cambios detectados en total:", total)
+print("Insertados:", added)
+print("Eliminados:", removed)
