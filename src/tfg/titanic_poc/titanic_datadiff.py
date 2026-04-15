1adb2ac93b3b9af0a1bd14e4dfc492844939c31e
Xavi Planas <xavi.planas.peres@gmail.com>
2026-04-15 10:52:19 +0200

    Compatible con clase Config

--- src/tfg/titanic_poc/titanic_datadiff.py
+++ src/tfg/titanic_poc/titanic_datadiff.py
@@ -1,32 +1,27 @@
-from tfg.titanic_poc.titanic_utils import mysql_engine, mysql_engine_url, postgres_engine_url, dataset 
-from data_diff import connect_to_table, diff_tables, disable_tracking
+from .titanic_utils import Config
+from data_diff import connect_to_table, diff_tables
 from sqlalchemy import inspect
 
 from tfg.datadiff_classifier.classifier import DiffClassifier
 from tfg.datadiff_classifier.models import DiffRow
 
+table_raw = Config.dataset["raw"]["table"]
+table_modified = Config.dataset["modified"]["table"]
+primary_key = "PassengerId"
 
-disable_tracking()
-
-DEBUG = True
-
-table_raw = dataset["raw"]["table"]
-table_modified = dataset["modified"]["table"]
-primary_key = ("PassengerId")
-
-mysql_metadata = inspect(mysql_engine)
+mysql_metadata = inspect(Config.mysql_engine)
 mysql_all_columns=[col["name"] for col in mysql_metadata.get_columns(table_raw)]
 #mysql_all_columns = []
 
 table_mysql = connect_to_table(
-    f"mysql://{mysql_engine_url}",
+    f"mysql://{Config.mysql_engine_url}",
     table_name=table_raw,
     key_columns=primary_key,
     extra_columns = mysql_all_columns
 )
 
 table_pg = connect_to_table(
-    f"postgresql://{postgres_engine_url}",
+    f"postgresql://{Config.postgres_engine_url}",
     table_name=table_modified,
     key_columns=("PassengerId"),
     extra_columns = mysql_all_columns
@@ -35,8 +30,8 @@ table_pg = connect_to_table(
 added = 0
 removed = 0
 total = 0
-left = []
-right = []
+left = {}
+right = {}
 
 #for diff in diff_tables(table_mysql, table_pg, extra_columns=mysql_all_columns):
 for diff in diff_tables(table_mysql, table_pg):
@@ -44,20 +39,37 @@ for diff in diff_tables(table_mysql, table_pg):
    d = dict(zip(mysql_all_columns, diff[1][1:]))
    if diff[0] == '+' :
       added+=1
-      right.append(d)
-      DEBUG and print(f"postgreSQL {d}")
+      right[d[primary_key]]=d 
+      Config.DEBUG and print(f"postgreSQL {d}")
    elif diff[0] == '-' :
       removed+=1
-      left.append(d)
-      DEBUG and print(f"mySQL {d}")
+      left[d[primary_key]]=d 
+      Config.DEBUG and print(f"mySQL {d}")
 
-   DEBUG and print(diff)
+   Config.DEBUG and print(diff)
 
-print("Cambios detectados en total:", total)
-print("Insertados:", added)
-print("Eliminados:", removed)
-
+if Config.DEBUG : 
+   print(60*'=')
+   print("Cambios detectados en total:", total)
+   print("Insertados:", added)
+   print("Eliminados:", removed)
+   print(60*'-')
+   print(f"\nLeft: {left}\nRight: {right}")
+   print(60*'=')
+   
 filas_a_comparar = DiffRow(primary_key, left, right, "mysql", "postgresql")
 clasificador_diff = DiffClassifier("diff 1")
 resultado = clasificador_diff.classify(filas_a_comparar)
 print(resultado)
+
+# filas_a_comparar = DiffRow(primary_key, left["269"], right["269"], "mysql", "postgresql")
+# resultado = clasificador_diff.classify(filas_a_comparar)
+# print(resultado["response"])
+
+# filas_a_comparar = DiffRow(primary_key, left["838"], right["838"], "mysql", "postgresql")
+# resultado = clasificador_diff.classify(filas_a_comparar)
+# print(resultado["response"])
+
+# filas_a_comparar = DiffRow(primary_key, left["84"], right["84"], "mysql", "postgresql")
+# resultado = clasificador_diff.classify(filas_a_comparar)
+# print(resultado["response"])
